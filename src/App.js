acb88bc92005454808d1bb3bbfc79d7a7bf9a473
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2024-03-26 13:52:34 +0300

    add inputFields

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [ingrediant, setIngrediant] = useState('');
+  const [array, setArray] = useState([]);
+
+  const handleAddNote = () => {
+    if (inputValue.trim() !== '') {
+      setNotes([...array, { title: '', ingrediant: '' }]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteNote = (index) => {
+    const newArray = [...array];
+    newArray.splice(index, 1);
+    setArray(newArray);
+  };
+
+  const handleEditNote = (index, updatedArraye) => {
+    const newArray = [...array];
+    newArray[index] = updatedArraye;
+    setArray(newArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app-container'>
+        <h1 className='header'>My Recipe</h1>
+        <input
+          className='inputField'
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter title"
+        />
+        <input
+          className='inputField'
+          type="text"
+          value={ingrediant}
+          onChange={(e) => setIngrediant(e.target.value)}
+          placeholder="Enter title"
+        />
+        <button className='add' onClick={handleAddNote}>Add Note</button>
+      </div>
     </div>
   );
 }
